16bfd9b74a8c26e6191fab3c4ddef54e7ce085b2
UNHOT <Serverings@protonmail.com>
2023-05-14 12:30:17 +0330

    First Commit.

---
+++ Components/Footer.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
+      <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
+        <div className="sm:flex sm:items-center sm:justify-between">
+          <iframe
+            width="200"
+            height="200"
+            loading="lazy"
+            allowFullScreen
+            referrerPolicy="no-referrer-when-downgrade"
+            src="https://www.google.com/maps/embed/v1/place?key=API_KEY
+    &q=Space+Needle,Seattle+WA"
+          ></iframe>
+
+          <ul className="flex flex-col items-start mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
+            <li className="my-1">
+              <a href="#" className="mr-4 hover:underline md:mr-6 ">
+                درباره ما
+              </a>
+            </li>
+            <li className="my-1">
+              <a href="#" className="mr-4 hover:underline md:mr-6">
+                آدرس
+              </a>
+            </li>
+            <li className="my-1">
+              <a href="#" className="mr-4 hover:underline md:mr-6 ">
+                شماره تلفن
+              </a>
+            </li>
+            <li className="my-1">
+              <a href="#" className="hover:underline">
+                ایمیل
+              </a>
+            </li>
+          </ul>
+        </div>
+        <a
+          href="https://farazamin.com/"
+          className="flex items-center justify-center mb-4 sm:mb-0"
+        >
+          <img
+            src="https://flowbite.com/docs/images/logo.svg"
+            className="h-8 mr-3"
+            alt="farazamin Logo"
+          />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            Farazamin
+          </span>
+        </a>
+        <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
+        <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">
+          © 2023{" "}
+          <a href="https://farazamin.com/" className="hover:underline">
+            Farazamin™
+          </a>
+          . All Rights Reserved.
+        </span>
+      </div>
+    </footer>
+  );
+}
---
+++ Components/Header.tsx
@@ -0,0 +1,72 @@
+import React from "react";
+import HeaderItem from "./HeaderItem";
+import Image from "next/image";
+
+export default function Header() {
+  const headerItems = [
+    "خانه",
+    "استخدام",
+    "استارت آپ های اجرا شده",
+    "فضای کار اشتراکی",
+    "شتابدهی و سرمایه گزاری",
+  ];
+  return (
+    <header>
+      <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5 dark:bg-gray-800">
+        <div className="flex flex-wrap flex-row-reverse justify-start items-center mx-auto max-w-screen-xl">
+          <a href="https://farazamin.com" className="flex items-center">
+            <Image width={50} height={50} src={"https://flowbite.com/docs/images/logo.svg"} className="mr-3 h-6 sm:h-9" alt="farazamin Logo"/>
+            <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">
+              Farazamin
+            </span>
+          </a>
+          <div className="flex items-center lg:order-2">
+            <button
+              data-collapse-toggle="mobile-menu-2"
+              type="button"
+              className="inline-flex items-center p-2 ml-1 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="mobile-menu-2"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="w-6 h-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clipRule="evenodd"
+                />
+              </svg>
+              <svg
+                className="hidden w-6 h-6"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clipRule="evenodd"
+                />
+              </svg>
+            </button>
+          </div>
+          <div
+            className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+            id="mobile-menu-2"
+          >
+            <ul className="flex flex-col-reverse mt-4 font-medium lg:flex-row-reverse lg:space-x-8 lg:mt-0">
+              {headerItems.map((headerItem) => (
+                <HeaderItem key={headerItem} itemName={headerItem} />
+              ))}
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
+}
