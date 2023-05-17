b47b215836e0fafc8e275c237a13181617005d22
yoonesZi82 <130362861+yoonesZi82@users.noreply.github.com>
2023-05-17 19:18:52 +0330

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+    "./public/*.html" ,
+    "./component/*.tsx" ,
+],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+      colors : {
+        'aliceblue' : '#f0f8ff' ,
+        'dark' : '#333',
+        'black' : '#434343' ,
+        'gray' : '#bdc3c7' ,
+        'darkblue' : '#082f49' ,
+        'glass' : 'rgba(255,255,255,0.1)'
+        },
+        spacing : {
+          '12%' : '12%' ,
+          '20%' : '20%' ,
+          '25%' : '25%' ,
+          '35%' : '35%' ,
+          '40%' : '40%' ,
+          '50%' : '50%' ,
+          '60%' : '60%' ,
+        } ,
+        boxShadow : {
+          'shadow1' : '0px 0px 5px '
+        }
     },
   },
   plugins: [],
 }
+
