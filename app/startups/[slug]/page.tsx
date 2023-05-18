9ed7738c4b66a3b413103e674c90bbfe93395998
UNHOT <Serverings@protonmail.com>
2023-05-18 13:01:32 +0330

    Fix.

--- app/startups/[slug]/page.tsx
+++ app/startups/[slug]/page.tsx
@@ -82,9 +82,9 @@ export default function slider({
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M15 19l-7-7 7-7"
               ></path>
             </svg>
@@ -106,9 +106,9 @@ export default function slider({
               xmlns="http://www.w3.org/2000/svg"
             >
               <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
                 d="M9 5l7 7-7 7"
               ></path>
             </svg>
