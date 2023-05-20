1f30e6922aceee51cffa4197ca9729cfe80a7a3b
UNHOT <Serverings@protonmail.com>
2023-05-20 09:58:44 +0330

    Minor fix.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,10 @@
-import Footer from "@/Components/footer/Footer";
-import Header from "@/Components/header/Header";
 import React from "react";
 
 export default function Home() {
   return (
     <main>
-      <Header />
       <div className="h-screen">Body!</div>
-      <Footer />
+
     </main>
   );
 }
