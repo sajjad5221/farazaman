331e9d7f3c9c13b3f549452827f40cadba07ba9c
Meraj Bighamian <merajbighamian@gmail.com>
2023-05-21 19:35:24 +0430

    add submitData function

--- Components/form/Input.tsx
+++ Components/form/Input.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React , {useRef} from 'react'
 
 interface Inputs{
     type : string,
-    label : string
+    label : string,
+    setName : string,
+
 }
 export default function Input(Props : Inputs) {
-  const {type,label} = Props;
+  const {type,label,setName} = Props;
+  const refName = useRef(null);
+
   return (
     <div>
 
@@ -14,7 +18,7 @@ export default function Input(Props : Inputs) {
     <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
         <svg aria-hidden="true" className="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg>
     </div>
-    <input type={type} id="input-group-1" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required />
+    <input ref={refName} type={type} id="input-group-1" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@flowbite.com" required name={setName}/>
     </div>
     
 </div>
--- app/form/page.tsx
+++ app/form/page.tsx
@@ -1,31 +1,42 @@
 import Footer from "@/Components/footer/Footer";
 import Header from "@/Components/header/Header";
 import Input from "@/Components/form/Input";
+
 import React from "react";
 
 export default function page() {
-  return (
-    <>
+  const submitData = async () =>{
+    const headers = { 'Content-Type': 'application/json' }
+    const res = await fetch("/startup-submit", {
+      headers,
+      method: 'POST',
+      body: JSON.stringify({
+        "data":"data2"
+      }),
+    })
+  }
+ 
 
+
+
+  return (
     <div className="ltr">
       <h2>فرم</h2>
       <p>توضیحات اولیه</p>
-      <form className="w-1/2 border mx-auto border-slate-50 rounded-sm whitespace-nowrap p-8" method="post" action={"/startup-submit"}>
-        <Input label={"نام و نام خانوادگی"} type="text"/>
-        <Input label={"شماره تماس"} type="number" />
-        <Input label={"تعداد اعضا"} type="number" />
-        <Input label={"پیچ تک"} type="file" />
-        
+      <form className="w-1/2 border mx-auto border-slate-50 rounded-sm whitespace-nowrap p-8" method="post" action="/startup-submit">
+        <Input label={"نام استارتاپ"} type="text" setName="name"/>
+        <Input label={"شماره تماس"} type="number" setName="phone"/>
+        <Input label={"تعداد اعضا"} type="number" setName="member_count"/>
+        <Input label={"ایمیل"} type="file" setName="email"/>
         <div className="flex items-start mb-6"></div>
         <button
           type="submit"
           className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          name="pitch"
         >
           Submit
         </button>
       </form>
     </div>
-
-    </>
   );
 }
