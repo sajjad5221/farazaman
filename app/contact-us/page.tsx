e94231b6b510f59676b598527a9ac52626fd7b1e
Meraj Bighamian <merajbighamian@gmail.com>
2023-05-23 21:26:52 +0430

    add contact us component and write it , add contact-us route and contact us page

---
+++ app/contact-us/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ContactUs from '@/Components/contact-us/contactForm'
+export default function page() {
+  return (
+    <div>
+      <ContactUs/>  
+
+    </div>
+  )
+}
