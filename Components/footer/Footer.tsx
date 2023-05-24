775785b8e845f529f9beb153f5f24845afe731f3
farazzkamali <farazzkamali80@gmail.com>
2023-05-24 02:54:01 -0400

    feat: add map

--- Components/footer/Footer.tsx
+++ Components/footer/Footer.tsx
@@ -6,16 +6,8 @@ export default function Footer() {
     <footer className="bg-white rounded-lg shadow dark:bg-gray-900 m-4">
       <div className="w-full max-w-screen-xl mx-auto p-4 md:py-8">
         <div className="sm:flex sm:items-center sm:justify-between">
-          <iframe
-            name="map"
-            width="200"
-            height="200"
-            loading="lazy"
-            allowFullScreen
-            referrerPolicy="no-referrer-when-downgrade"
-            src="https://www.google.com/maps/embed/v1/place?key=API_KEY
-    &q=Space+Needle,Seattle+WA"
-          ></iframe>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d480.3047437285542!2d51.667246131748406!3d32.62261962506355!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3fbc37b19cd0de21%3A0xd88964e49361525!2sTala%20complex!5e0!3m2!1sen!2s!4v1684834056278!5m2!1sen!2s" width="600" height="450" loading="lazy"></iframe>
+
 
           <ul className="flex flex-col items-start mb-6 text-sm font-medium text-gray-500 sm:mb-0 dark:text-gray-400">
             <li className="my-1">
