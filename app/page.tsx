aaa97fde3e224413fc06d50f4ae1b37fb88827cc
UNHOT <Serverings@protonmail.com>
2023-05-27 10:55:30 +0330

    Add hired experiences.

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,10 @@ import Customers from "@/Components/Customers";
 import Faq from "@/Components/Faqs";
 import { Features } from "@/Components/Features";
 import { Hero } from "@/Components/Hero";
-import Pricing from "@/Components/Pricing";
+import Pricing from "@/Components/StartUps";
 import { Services } from "@/Components/Services";
 import React from "react";
+import SliderMenu from "@/Components/SliderMenu";
 
 export default function Home() {
   return (
@@ -15,6 +16,11 @@ export default function Home() {
         <Features />
         <Pricing />
         <Faq />
+        <SliderMenu
+          type="exp"
+          title="تجربه استخدامی ها"
+          description="لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز"
+        />
         <Customers />
       </div>
     </main>
