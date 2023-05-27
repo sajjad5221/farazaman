7d04f10ac8d871fcae6c2ed72364b098d5ef285c
UNHOT <Serverings@protonmail.com>
2023-05-27 12:25:45 +0330

    Minor fix.

--- Components/Customers.tsx
+++ Components/Customers.tsx
@@ -2,72 +2,75 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 const customers = [
-    {
-        name: "google-play",
-        href: "https://play.google.com/",
-        src: "https://via.placeholder.com/150",
-        alt: "Google play logo",
-    },
-    {
-        name: "github",
-        href: "https://github.com/",
-        src: "https://via.placeholder.com/150",
-        alt: "Github logo",
-    },
-    {
-        name: "docker",
-        href: "https://www.docker.com/",
-        src: "https://via.placeholder.com/150",
-        alt: "Docker logo",
-    },
-    {
-        name: "python",
-        href: "https://www.python.org/",
-        src: "https://via.placeholder.com/150",
-        alt: "Python logo",
-    },
-    {
-        name: "slack",
-        href: "https://slack.com/",
-        src: "https://via.placeholder.com/150",
-        alt: "Slack logo",
-    },
-    {
-        name: "discord",
-        href: "https://discord.com/",
-        src: "https://via.placeholder.com/150",
-        alt: "Discord logo",
-    }
-]
+  {
+    name: "google-play",
+    href: "https://play.google.com/",
+    src: "https://via.placeholder.com/150",
+    alt: "Google play logo",
+  },
+  {
+    name: "github",
+    href: "https://github.com/",
+    src: "https://via.placeholder.com/150",
+    alt: "Github logo",
+  },
+  {
+    name: "docker",
+    href: "https://www.docker.com/",
+    src: "https://via.placeholder.com/150",
+    alt: "Docker logo",
+  },
+  {
+    name: "python",
+    href: "https://www.python.org/",
+    src: "https://via.placeholder.com/150",
+    alt: "Python logo",
+  },
+  {
+    name: "slack",
+    href: "https://slack.com/",
+    src: "https://via.placeholder.com/150",
+    alt: "Slack logo",
+  },
+  {
+    name: "discord",
+    href: "https://discord.com/",
+    src: "https://via.placeholder.com/150",
+    alt: "Discord logo",
+  },
+];
 
 export default function Customers() {
-    return (
-        <div className="bg-white dark:bg-neutral-900 w-full px-4 pt-16 pb-16" id="faq">
-            <h2 className="text-4xl font-bold text-center">
-                مشتری ها
-            </h2>
+  return (
+    <div
+      className="bg-white dark:bg-neutral-900 w-full px-4 pt-16 pb-16"
+      id="faq"
+    >
+      <h2 className="text-4xl font-bold text-center">شرکای تجاری </h2>
 
-            <p className="pt-6 pb-8 text-base max-w-2xl text-center m-auto dark:text-neutral-400">
-            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز
-            </p>
-            <div className="mx-auto w-full max-w-4xl bg-white dark:bg-transparent">
-                <div className="text-center justify-center items-center grid grid-cols-2 sm:grid-cols-3 md:grid-cols-6">
-                    {customers.map((item) => (
-                        <Link key={item.name} href={item.href} target="_blank">
-                            <Image
-                                className="h-10 w-auto mt-6 justify-center text-center mx-auto hover:scale-110 transition"
-                                src={item.src}
-                                alt={item.alt}
-                                width={150}
-                                height={150}
-                                quality={75}
-                                sizes="100vw"
-                            />
-                        </Link>
-                    ))}
-                </div>
-            </div>
+      <p className="pt-6 pb-8 text-base max-w-2xl text-center m-auto dark:text-neutral-400">
+        لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده
+        از طراحان گرافیک است چاپگرها و متون بلکه روزنامه و مجله در ستون و
+        سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز
+      </p>
+      <div className="mx-auto w-full max-w-4xl bg-white dark:bg-transparent">
+        <div className="text-center justify-center items-center grid grid-cols-2 sm:grid-cols-3 md:grid-cols-6">
+          {customers.map((item) => (
+            <Link key={item.name} href={item.href} target="_blank">
+              <Image
+                className="h-10 w-auto mt-6 justify-center text-center mx-auto hover:scale-110 transition rounded-full mb-1"
+                src={item.src}
+                alt={item.alt}
+                width={150}
+                height={150}
+                quality={75}
+                sizes="100vw"
+              />
+              <span>نام شرکت</span>
+            </Link>
+          ))}
         </div>
-
-    );
+      </div>
+    </div>
+  );
 }
--- Components/SliderMenu.tsx
+++ Components/SliderMenu.tsx
@@ -18,61 +18,61 @@ export default function SliderMenu({
   const expriences = [
     {
       id: 1,
-      title: "Hired Name",
+      title: "Name",
       description: "This is the description for Card 1.",
       imageUrl: "https://via.placeholder.com/150",
     },
     {
       id: 1,
-      title: "Hired Name",
+      title: "Name",
       description: "This is the description for Card 1.",
       imageUrl: "https://via.placeholder.com/150",
     },
     {
       id: 1,
-      title: "Hired Name",
+      title: "Name",
       description: "This is the description for Card 1.",
       imageUrl: "https://via.placeholder.com/150",
     },
     {
       id: 1,
-      title: "Hired Name",
+      title: "Name",
       description: "This is the description for Card 1.",
       imageUrl: "https://via.placeholder.com/150",
     },
     {
       id: 1,
-      title: "Hired Name",
+      title: "Name",
       description: "This is the description for Card 1.",
       imageUrl: "https://via.placeholder.com/150",
     },
     {
       id: 1,
-      title: "Hired Name",
+      title: "Name",
       description: "This is the description for Card 1.",
       imageUrl: "https://via.placeholder.com/150",
     },
     {
       id: 1,
-      title: "Hired Name",
+      title: "Name",
       description: "This is the description for Card 1.",
       imageUrl: "https://via.placeholder.com/150",
     },
     {
       id: 1,
-      title: "Hired Name",
+      title: "Name",
       description: "This is the description for Card 1.",
       imageUrl: "https://via.placeholder.com/150",
     },
     {
       id: 1,
-      title: "Hired Name",
+      title: "Name",
       description: "This is the description for Card 1.",
       imageUrl: "https://via.placeholder.com/150",
     },
     {
       id: 1,
-      title: "Hired Name",
+      title: "Name",
       description: "This is the description for Card 1.",
       imageUrl: "https://via.placeholder.com/150",
     },
@@ -81,69 +81,69 @@ export default function SliderMenu({
   const StartUps = [
     {
       id: 1,
-      title: "Hired Name",
+      title: "Name",
       description: "This is the description for Card 1.",
       imageUrl: "https://via.placeholder.com/150",
     },
     {
       id: 1,
-      title: "Hired Name",
+      title: "Name",
       description: "This is the description for Card 1.",
       imageUrl: "https://via.placeholder.com/150",
     },
     {
       id: 1,
-      title: "Hired Name",
+      title: "Name",
       description: "This is the description for Card 1.",
       imageUrl: "https://via.placeholder.com/150",
     },
     {
       id: 1,
-      title: "Hired Name",
+      title: "Name",
       description: "This is the description for Card 1.",
       imageUrl: "https://via.placeholder.com/150",
     },
     {
       id: 1,
-      title: "Hired Name",
+      title: "Name",
       description: "This is the description for Card 1.",
       imageUrl: "https://via.placeholder.com/150",
     },
     {
       id: 1,
-      title: "Hired Name",
+      title: "Name",
       description: "This is the description for Card 1.",
       imageUrl: "https://via.placeholder.com/150",
     },
     {
       id: 1,
-      title: "Hired Name",
+      title: "Name",
       description: "This is the description for Card 1.",
       imageUrl: "https://via.placeholder.com/150",
     },
     {
       id: 1,
-      title: "Hired Name",
+      title: "Name",
       description: "This is the description for Card 1.",
       imageUrl: "https://via.placeholder.com/150",
     },
     {
       id: 1,
-      title: "Hired Name",
+      title: "Name",
       description: "This is the description for Card 1.",
       imageUrl: "https://via.placeholder.com/150",
     },
     {
       id: 1,
-      title: "Hired Name",
+      title: "Name",
       description: "This is the description for Card 1.",
       imageUrl: "https://via.placeholder.com/150",
     },
   ];
-  type BillingInterval = "year" | "month";
+  type BillingInterval = "doing" | "done";
 
   const [billingInterval, setBillingInterval] =
-    useState<BillingInterval>("month");
+    useState<BillingInterval>("doing");
   const carouselRef: React.RefObject<HTMLDivElement> = useRef(null);
 
   const handleNextSlide = () => {
@@ -172,10 +172,10 @@ export default function SliderMenu({
         <div className="mx-auto px-4 sm:px-6 lg:px-8 sm:flex sm:flex-col sm:align-center">
           <div className="relative self-center text-base font-semibold mb-6 bg-neutral-200  dark:bg-neutral-800 rounded-lg flex sm:mb-8">
             <button
-              onClick={() => setBillingInterval("month")}
+              onClick={() => setBillingInterval("doing")}
               type="button"
               className={`${
-                billingInterval === "month"
+                billingInterval === "doing"
                   ? "relative w-1/2 bg-white dark:bg-neutral-600 text-neutral-900 dark:text-neutral-100"
                   : "ml-0.5 relative w-1/2 text-neutral-900 dark:text-neutral-400"
               } rounded-md m-1 py-2 whitespace-nowrap sm:w-auto sm:px-8`}
@@ -183,10 +183,10 @@ export default function SliderMenu({
               انجام شده
             </button>
             <button
-              onClick={() => setBillingInterval("year")}
+              onClick={() => setBillingInterval("done")}
               type="button"
               className={`${
-                billingInterval === "year"
+                billingInterval === "done"
                   ? "relative w-1/2 bg-white dark:bg-neutral-600 text-neutral-900 dark:text-neutral-100"
                   : "ml-0.5 relative w-1/2 text-neutral-900 dark:text-neutral-400"
               } rounded-md m-1 py-2 whitespace-nowrap sm:w-auto sm:px-8`}
@@ -198,69 +198,135 @@ export default function SliderMenu({
       ) : (
         ""
       )}
-      <div className="flex flex-row items-center">
-        <button
-          className="rounded-full bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-1.5 px-1.5"
-          onClick={handlePrevSlide}
-        >
-          <ChevronLeftIcon className="h-6 w-6" />
-        </button>
+      {billingInterval === "done" ? (
+        <div className="flex flex-row items-center">
+          <button
+            className="rounded-full bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-1.5 px-1.5"
+            onClick={handlePrevSlide}
+          >
+            <ChevronLeftIcon className="h-6 w-6" />
+          </button>
 
-        <div
-          className="flex overflow-x-auto whitespace-nowrap"
-          ref={carouselRef}
-        >
-          {type === "exp"
-            ? expriences.map((card) => (
-                <div
-                  key={card.id}
-                  className="flex flex-col justify-evenly w-64 h-64 bg-neutral-100 dark:bg-neutral-800 rounded-lg p-4 cursor-pointer mx-2"
-                  style={{ scrollSnapAlign: "start" }}
-                >
-                  <Image
-                    width={80}
-                    height={80}
-                    quality={100}
-                    src={card.imageUrl}
-                    alt={card.title}
-                    className="mx-auto rounded-full"
-                  />
+          <div
+            className="flex overflow-x-auto whitespace-nowrap"
+            ref={carouselRef}
+          >
+            {type === "exp"
+              ? expriences.map((card) => (
+                  <div
+                    key={card.id}
+                    className="flex flex-col justify-evenly w-64 h-64 bg-neutral-100 dark:bg-neutral-800 rounded-lg p-4 cursor-pointer mx-2"
+                    style={{ scrollSnapAlign: "start" }}
+                  >
+                    <Image
+                      width={80}
+                      height={80}
+                      quality={100}
+                      src={card.imageUrl}
+                      alt={card.title}
+                      className="mx-auto rounded-full"
+                    />
 
-                  <h3 className="text-center mt-2 font-bold">{card.title}</h3>
-                  <p className="text-center dark:text-neutral-400">
-                    {card.description}
-                  </p>
-                </div>
-              ))
-            : StartUps.map((card) => (
-                <div
-                  key={card.id}
-                  className="flex flex-col justify-evenly w-64 h-64 bg-neutral-100 dark:bg-neutral-800 rounded-lg p-4 cursor-pointer mx-2"
-                  style={{ scrollSnapAlign: "start" }}
-                >
-                  <Image
-                    width={80}
-                    height={80}
-                    quality={100}
-                    src={card.imageUrl}
-                    alt={card.title}
-                    className="mx-auto rounded-full"
-                  />
+                    <h3 className="text-center mt-2 font-bold">{card.title}</h3>
+                    <p className="text-center dark:text-neutral-400">
+                      {card.description}
+                    </p>
+                  </div>
+                ))
+              : StartUps.map((card) => (
+                  <div
+                    key={card.id}
+                    className="flex flex-col justify-evenly w-64 h-64 bg-neutral-50 dark:bg-neutral-700 rounded-lg p-4 cursor-pointer mx-2"
+                    style={{ scrollSnapAlign: "start" }}
+                  >
+                    <Image
+                      width={80}
+                      height={80}
+                      quality={100}
+                      src={card.imageUrl}
+                      alt={card.title}
+                      className="mx-auto rounded-full"
+                    />
 
-                  <h3 className="text-center mt-2 font-bold">{card.title}</h3>
-                  <p className="text-center dark:text-neutral-400">
-                    {card.description}
-                  </p>
-                </div>
-              ))}
+                    <h3 className="text-center mt-2 font-bold">{card.title}</h3>
+                    <p className="text-center dark:text-neutral-400">
+                      {card.description}
+                    </p>
+                  </div>
+                ))}
+          </div>
+          <button
+            className="rounded-full bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-1.5 px-1.5"
+            onClick={handleNextSlide}
+          >
+            <ChevronRightIcon className="h-6 w-6" />
+          </button>
         </div>
-        <button
-          className="rounded-full bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-1.5 px-1.5"
-          onClick={handleNextSlide}
-        >
-          <ChevronRightIcon className="h-6 w-6" />
-        </button>
-      </div>
+      ) : (
+        <div className="flex flex-row items-center">
+          <button
+            className="rounded-full bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-1.5 px-1.5"
+            onClick={handlePrevSlide}
+          >
+            <ChevronLeftIcon className="h-6 w-6" />
+          </button>
+
+          <div
+            className="flex overflow-x-auto whitespace-nowrap"
+            ref={carouselRef}
+          >
+            {type === "exp"
+              ? expriences.map((card) => (
+                  <div
+                    key={card.id}
+                    className="flex flex-col justify-evenly w-64 h-64 bg-neutral-100 dark:bg-neutral-800 rounded-lg p-4 cursor-pointer mx-2"
+                    style={{ scrollSnapAlign: "start" }}
+                  >
+                    <Image
+                      width={80}
+                      height={80}
+                      quality={100}
+                      src={card.imageUrl}
+                      alt={card.title}
+                      className="mx-auto rounded-full"
+                    />
+
+                    <h3 className="text-center mt-2 font-bold">{card.title}</h3>
+                    <p className="text-center dark:text-neutral-400">
+                      {card.description}
+                    </p>
+                  </div>
+                ))
+              : StartUps.map((card) => (
+                  <div
+                    key={card.id}
+                    className="flex flex-col justify-evenly w-64 h-64 bg-neutral-100 dark:bg-neutral-800 rounded-lg p-4 cursor-pointer mx-2"
+                    style={{ scrollSnapAlign: "start" }}
+                  >
+                    <Image
+                      width={80}
+                      height={80}
+                      quality={100}
+                      src={card.imageUrl}
+                      alt={card.title}
+                      className="mx-auto rounded-full"
+                    />
+
+                    <h3 className="text-center mt-2 font-bold">{card.title}</h3>
+                    <p className="text-center dark:text-neutral-400">
+                      {card.description}
+                    </p>
+                  </div>
+                ))}
+          </div>
+          <button
+            className="rounded-full bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-1.5 px-1.5"
+            onClick={handleNextSlide}
+          >
+            <ChevronRightIcon className="h-6 w-6" />
+          </button>
+        </div>
+      )}
     </div>
   );
 }
