c3d0ba339f7e4bd0a6162e3e60f5834d4c17b052
UNHOT <Serverings@protonmail.com>
2023-05-27 12:25:10 +0330

    Clean not working page.

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,23 +1,7 @@
-import React from "react";
-import tumbnailImage from "../public/img.jpg";
-import Cards from "../../Components/EventCards"
+import React from 'react'
 
-
-// Event Component for show list of events crad
-export default function Events() {
-
-  // test props information
-  let title: string = "استارتاپ";
-  let image: any = tumbnailImage;
-  let description: string = "این یک توضیحات یک استارتاپ است";
-
-  let cards = new Array(Cards({title,image,description}), Cards({title,image,description}), Cards({title,image,description}),Cards({title,image,description}), Cards({title,image,description}), Cards({title,image,description}));
-  let card: any;
+export default function page() {
   return (
-    <div className="flex flex-row flex-wrap justify-around">
-      {cards.map((card) => (
-        <div className="basis-1/3 justify-around  pl-12">{card}</div>
-      ))}
-    </div>
-  );
+    <div>page</div>
+  )
 }
