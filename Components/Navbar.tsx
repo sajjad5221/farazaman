57264efbc065082d8013b442131d3a20d2e13857
Meraj Bighamian <merajbighamian@gmail.com>
2023-05-28 19:53:26 +0430

    set dark mode of startups form

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -68,7 +68,6 @@ export default function Navbar() {
                       
                     ))}
                     <ThemeSwitchButton />
-                    <ThemeSwitchOption />
                   </div>
                 </div>
 
