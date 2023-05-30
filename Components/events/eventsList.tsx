271dab51cdd066247805838ae091b338da3614a1
Faraz <farazzkamali80@gmail.com>
2023-05-30 12:14:54 +0430

    create Event list

---
+++ Components/events/eventsList.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import EventCard from './eventsCard';
+
+type Props = {
+  events: Event[];
+};
+
+type Event = {
+    id: number;
+    title: string;
+    description: string;
+    date: string;
+    location: string;
+    image: string;
+  };
+
+const EventList = ({ events }: Props) => {
+  return (
+    <div className="flex flex-wrap -mx-4 mt-1">
+      {events.map((event) => (
+        <div key={event.id} className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 px-4 mb-8">
+          <EventCard event={event} />
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default EventList;
