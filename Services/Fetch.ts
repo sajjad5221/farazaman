5621694fb07a78d6d3b5c233b0872820aa550e3f
UNHOT <Serverings@protonmail.com>
2023-06-03 12:09:56 +0330

    Edit fetch function.

--- Services/Fetch.ts
+++ Services/Fetch.ts
@@ -1,13 +1,21 @@
 // import { config } from "./Url";
 
-// export default async function Fetch(endpoint: string) {
-//   const url = config.url;
+// export default async function Fetcher(url: string) {
+//   //   const url = config.url;
 
-//   const staticFetch: any = await fetch(url + endpoint).then((data) => {
+//   const staticFetch: any = await fetch(url).then((data) => {
 //     if (!data.ok) {
 //       throw new Error("Failed To Fetch Data.");
 //     }
-//     console.log(staticFetch.json());
 //     return staticFetch.json();
 //   });
 // }
+export default async function FetchApi(url: string) {
+  const data = fetch(url, {
+    method: "GET",
+    headers: { "content-type": "application/json" },
+  });
+  const parsedData: any = (await data).json();
+
+  return await parsedData;
+}
