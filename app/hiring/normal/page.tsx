d025b1c04bd01d0141bf9e8a23611d526bd13287
Sajjad Momeni <sajjad5221@gmail.com>
2023-06-04 10:45:34 +0330

    chore: add HiringForm component

--- app/hiring/normal/page.tsx
+++ app/hiring/normal/page.tsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import HiringForm from '@/Components/hiring/HiringForm'
 
 export default function page() {
   return (
-    <div>page</div>
+    <div className='container'>
+      <HiringForm/>
+    </div>
   )
 }
