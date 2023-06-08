ad3f901332f62bc477380ea2489cecc42cbf2d26
Meraj Bighamian <merajbighamian@gmail.com>
2023-06-08 13:56:27 +0430

    set validation for startups form and show success message for contact-us  and startups form

--- Components/contact-us/contactForm.tsx
+++ Components/contact-us/contactForm.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import React, { useState, useEffect } from "react";
 import GetCsrfToken from "@/Services/GetCsrfToken";
 import axios from "axios";
@@ -19,25 +19,31 @@ const ContactUs = () => {
     message: "",
   });
 
-  []
+  [];
   const {
     register,
     handleSubmit,
-    reset,
     formState: { errors },
+    reset,
   } = useForm<Info>({
     mode: "onBlur",
   });
 
-  const [isSubmitting,setIsSubmitting] = useState(false)
-  
-  const [isSuccess, setIsSuccess] = React.useState(false);
-  const [Message, setMessage] = React.useState("");
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement> | React.ChangeEvent<HTMLTextAreaElement>) => {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [Send, setSend] = useState(false);
+  const [isSuccess, setIsSuccess] = useState(false);
+  const [Message, setMessage] = useState("");
+  const handleChange = (
+    e:
+      | React.ChangeEvent<HTMLInputElement>
+      | React.ChangeEvent<HTMLTextAreaElement>
+  ) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
   const onSubmit = async (data: Info) => {
+    setSend(true);
+    setIsSubmitting(true);
     try {
       const response = await axios.post(
         "http://localhost:8000/contact/",
@@ -49,19 +55,18 @@ const ContactUs = () => {
           },
         }
       );
-      const json = response.data;
-      if (json.success) {
-        setIsSuccess(true);
-        setMessage(json.message);
-        console.log(response.data);
-        reset();
-      } else {
-        setIsSuccess(false);
-        setMessage(json.message);
-      }
+      setIsSuccess(true);
+      setMessage("ارسال موفقیت آمیز بود");
+      setSend(false);
+      reset(); // Reset the form field
+
     } catch (error) {
+      console.log(error);
+      setMessage("ارسال ناموفق بود !");
+      setSend(false);
       setIsSuccess(false);
     }
+
   };
 
   const [csrfToken, setCsrfToken] = useState("");
@@ -77,22 +82,30 @@ const ContactUs = () => {
   return (
     <div className="bg-gray-50 dark:bg-neutral-900 mt-16" id="contact">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-16 text-center">
-        <h2 className="text-black text-4xl font-bold dark:text-white">ارتباط با ما</h2>
+        <h2 className="text-black text-4xl font-bold dark:text-white">
+          ارتباط با ما
+        </h2>
 
         <p className="pt-6 pb-6 text-base max-w-2xl text-center m-auto text-gray-700 dark:text-neutral-400">
-          برای ارتباط با ما میتوانید فرم زیر را پر کنید تا کارشناسان ما هرچه سریعتر با شما تماس برقرار نمایند
+          برای ارتباط با ما میتوانید فرم زیر را پر کنید تا کارشناسان ما هرچه
+          سریعتر با شما تماس برقرار نمایند
         </p>
       </div>
 
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-8 pb-16 grid md:grid-cols-2 lg:grid-cols-2 gap-y-8 md:gap-x-8 md:gap-y-8 lg:gap-x-8 lg:gap-y-16">
         <div>
-          <h2 className="text-lg font-bold text-black dark:text-white">ارتباط با شتابدهنده فرازمان</h2>
+          <h2 className="text-lg font-bold text-black dark:text-white">
+            ارتباط با شتابدهنده فرازمان
+          </h2>
           <p className="max-w-sm mt-4 mb-4 text-gray-600 dark:text-neutral-400">
-            برای ارتباط با تیم شتابدهنده فرازمان میتوانیداز طریق پر کردن فرم روبه رو یا روش های زیر اقدام نمایید.
+            برای ارتباط با تیم شتابدهنده فرازمان میتوانیداز طریق پر کردن فرم
+            روبه رو یا روش های زیر اقدام نمایید.
           </p>
 
           <div className="flex items-center mt-8 space-x-2 text-dark-600 dark:text-neutral-400">
-            <span className="text-gray-600 dark:text-neutral-400">ایران-اصفهان-خیابان سعادت آباد-مجتمع طلا- واحد c</span>
+            <span className="text-gray-600 dark:text-neutral-400">
+              ایران-اصفهان-خیابان سعادت آباد-مجتمع طلا- واحد c
+            </span>
           </div>
 
           <div className="flex items-center mt-2 space-x-2 text-gray-600 dark:text-neutral-400">
@@ -111,19 +124,24 @@ const ContactUs = () => {
                 type="text"
                 placeholder="نام و نام خانوادگی"
                 autoComplete="off"
-                value={formData.name}
-                
+
                 pattern="^[\u0600-\u06FF\s]+$"
                 className={`w-full px-4 py-3 border-2 placeholder:text-neutral-400 dark:text-white rounded-md outline-none dark:placeholder:text-neutral-200 dark:bg-neutral-900 focus:ring-4 ${
-                  errors.name ? "border-red-500" : ""
+                  errors.name ? "border-yellow-500" : ""
                 }`}
                 {...register("name", {
-                  required: "نام و نام خانوادگی را وارد کنید.",
+                  required: "نام استارتاپ را وارد کنید.",
+                  pattern: {
+                    value: /^[\u0600-\u06FF\s]+$/,
+                    message: "نام استارتاپ را به درستی وارد کنید.",
+                  },
                 })}
                 onChange={handleChange}
               />
               {errors.name && (
-                <span className="text-red-500 text-sm">{errors.name.message}</span>
+                <span className="text-yellow-500 text-sm">
+                  {errors.name.message}
+                </span>
               )}
             </div>
 
@@ -131,11 +149,9 @@ const ContactUs = () => {
               <input
                 type="email"
                 placeholder="farazaman@gmail.com"
-                value={formData.email}
-                
                 autoComplete="off"
                 className={`w-full px-4 py-3 border-2 placeholder:text-neutral-400 dark:text-white rounded-md outline-none dark:placeholder:text-neutral-200 dark:bg-neutral-900 focus:ring-4 ${
-                  errors.email ? "border-red-500" : ""
+                  errors.email ? "border-yellow-500" : ""
                 }`}
                 {...register("email", {
                   required: "آدرس ایمیل خود را وارد کنید.",
@@ -147,7 +163,9 @@ const ContactUs = () => {
                 onChange={handleChange}
               />
               {errors.email && (
-                <span className="text-red-500 text-sm">{errors.email.message}</span>
+                <span className="text-yellow-500 text-sm">
+                  {errors.email.message}
+                </span>
               )}
             </div>
 
@@ -156,10 +174,8 @@ const ContactUs = () => {
                 type="tel"
                 placeholder="091311111111"
                 autoComplete="off"
-                value={formData.phone}
-                
                 className={`w-full px-4 py-3 border-2 placeholder:text-neutral-400 dark:text-white rounded-md outline-none dark:placeholder:text-neutral-200 dark:bg-neutral-900 focus:ring-4 ${
-                  errors.phone ? "border-red-500" : ""
+                  errors.phone ? "border-yellow-500" : ""
                 }`}
                 {...register("phone", {
                   required: "شماره تماس را وارد کنید.",
@@ -171,17 +187,17 @@ const ContactUs = () => {
                 onChange={handleChange}
               />
               {errors.phone && (
-                <span className="text-red-500 text-sm">{errors.phone.message}</span>
+                <span className="text-yellow-500 text-sm">
+                  {errors.phone.message}
+                </span>
               )}
             </div>
 
             <div className="mb-3">
               <textarea
                 placeholder="پیام شما"
-                value={formData.message}
-                
                 className={`w-full px-4 py-3 border-2 placeholder:text-neutral-400 dark:text-white dark:placeholder:text-neutral-200 dark:bg-neutral-900 rounded-md outline-none h-36 focus:ring-4 ${
-                  errors.message ? "border-red-500" : ""
+                  errors.message ? "border-yellow-500" : ""
                 }`}
                 {...register("message", {
                   required: "متن پیام را وارد کنید.",
@@ -189,19 +205,69 @@ const ContactUs = () => {
                 onChange={handleChange}
               />
               {errors.message && (
-                <span className="text-red-500 text-sm">{errors.message.message}</span>
+                <span className="text-yellow-500 text-sm">
+                  {errors.message.message}
+                </span>
               )}
-              
             </div>
 
             <button
               type="submit"
-              disabled={isSubmitting}
+              disabled={Send}
               className="w-full py-4 font-semibold text-white transition-colors bg-neutral-900 rounded-md hover:bg-neutral-800 focus:outline-none focus:ring-offset-2 focus:ring focus:ring-neutral-200 px-7 dark:bg-white dark:text-black"
             >
-              {isSubmitting ? "در حال ارسال..." : "ارسال"}
+              {Send ? "در حال ارسال..." : "ارسال"}
             </button>
           </form>
+          {isSuccess && isSubmitting && (Message != "") && (
+            <div
+              className="flex p-4 mb-4 mt-6 text-sm text-bold text-green-900 rounded-lg bg-green-10 dark:bg-neutral-700 dark:text-green-400"
+              role="alert"
+            >
+              <svg
+                aria-hidden="true"
+                className="flex-shrink-0 inline w-5 h-5 mr-3"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+              <span className="sr-only">پیام</span>
+              <div>
+                <span className="font-medium">{Message}</span>!
+              </div>
+            </div>
+          )}
+
+          {!isSuccess && isSubmitting && (Message != "") && (
+            <div
+              className="flex p-4 mb-4 mt-6 text-sm text-bold text-red-900 rounded-lg bg-red-90 dark:bg-neutral-700 dark:text-red-400"
+              role="alert"
+            >
+              <svg
+                aria-hidden="true"
+                className="flex-shrink-0 inline w-5 h-5 mr-3"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+              <span className="sr-only">پیام</span>
+              <div>
+                <span className="font-medium">{Message}</span>!
+              </div>
+            </div>
+          )}
         </div>
       </div>
     </div>
