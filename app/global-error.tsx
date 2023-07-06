3576246c554f9c973a6a3d4c7abafc5538d063bd
UNHOT <Serverings@protonmail.com>
2023-07-06 10:32:52 +0330

    Reload on error instead of return to previous page.

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,11 +1,13 @@
-"use client";
+"use client"
+
 import Router from "next/router";
 import React from "react";
+
 export default function Error() {
   return (
     <div>
       <h2>Something went wrong!</h2>
-      <button onClick={Router.back}>Try again</button>
+      <button onClick={Router.reload}>Try again</button>
     </div>
   );
 }
