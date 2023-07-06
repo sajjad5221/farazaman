1c8fd4b07beae53cbef8d5fc25f70b54a8e2009d
UNHOT <Serverings@protonmail.com>
2023-07-06 10:22:12 +0330

    Refactor.

--- Services/GetFileNames.ts
+++ Services/GetFileNames.ts
@@ -1,9 +1,10 @@
-import fs from 'fs'
+import fs from 'fs';
 
-export default function GetFiles(id:string|number|undefined){
-  const folderPath = "public/static/images/startups/"+id+"/";
+export default function GetFiles(id: string | number | undefined) {
+  const folderPath = `public/static/images/startups/${id}/`;
 
+  // Read the files in the specified folder path
   const files = fs.readdirSync(folderPath);
-  return files;
 
+  return files;
 }
