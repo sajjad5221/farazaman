b481e90a25c67a67a2e0f41f7a9022d9242e0063
ali mokhtari <alimokhtari7902@gmail.com>
2023-12-26 14:58:30 +0330

    chore: add tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,22 +13,19 @@ module.exports = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
-      fontFamily:{
-        yakan: ["yekan", 'sans-serif']
+      fontFamily: {
+        yakan: ['yekan', 'sans-serif'],
       },
-      screens : {
-        's' : '500px' ,
-        'mobile' : '420px',
-        'mobile2' : '450px',
-        'little' : '350px',
-        'little2' : '360px',
-        'long' : '1350px',
-        'long2' : '1080px',
-        'long3' : '1010px',
-        'long4' : '850px',
-        'long5' : '767px',
-      },     
+      colors: {
+        brand: {
+          DEFAULT: '#D6A001',
+          light: '#F7ECCC',
+        },
+        customGray: {
+          DEFAULT: '#696969',
+        },
+      },
     },
   },
   plugins: [],
-}
+};
