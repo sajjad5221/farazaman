947439504e7a78711d81513a870090894d683e4a
ali mokhtari <alimokhtari7902@gmail.com>
2024-01-02 15:10:46 +0330

    chore: add services section

--- Components/home/ServiceCard.tsx
+++ Components/home/ServiceCard.tsx
@@ -1,7 +1,31 @@
 import React from 'react';
-import { useTranslation } from 'app/i18n';
-import { useLang } from '@/stores/langStore';
+import Circle from '../icons/Circle';
+import Image from 'next/image';
 
-export default async function ServiceCard() {
-  return <div></div>;
+export default async function ServiceCard({
+  title,
+  image,
+  alt,
+}: {
+  title: string;
+  image: string;
+  alt: string;
+}) {
+  return (
+    <div className="shadow-lg flex flex-col items-center px-1 py-4 m-4 rounded-lg w-36 md:w-52 justify-center mx-auto">
+      <div className="w-full h-32 md:w-40 md:h-40">
+        <Image
+          src={image}
+          alt={alt}
+          width={180}
+          height={160}
+          className="rounded-tr-[8px] rounded-br-[8px] rounded-bl-[8px] rounded-tl-[32px]"
+        />
+      </div>
+      <div className="flex items-center pt-2">
+        <Circle />
+        <p className="text-black pr-2 text-base">{title}</p>
+      </div>
+    </div>
+  );
 }
--- Components/icons/Circle.tsx
+++ Components/icons/Circle.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function Circle() {
-  return <div className="w-6 h-6 rounded-full bg-brand"></div>;
+  return <div className="w-5 h-5 rounded-full bg-brand"></div>;
 }
